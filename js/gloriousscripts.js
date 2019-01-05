090f6e3352d96318b771ebe1765ab09d5c2ca62d
17zhaoamy@gmail.com <17zhaoamy@gmail.com>
2019-01-04 16:22:29 -0800

    glorious demo

--- js/gloriousscripts.js
+++ js/gloriousscripts.js
@@ -16,20 +16,31 @@ demo
     })
     .respond('How many men and women do you want paired off?')
     .command('3', {
-        promptString: '',
-        onCompleteDelay: 500
+        onCompleteDelay: 2000
     })
     .respond('What is man 1\'s preference list?')
-    .command('2 1 3')
+    .command('2 1 3', {
+        onCompleteDelay: 1500
+    })
     .respond('What is man 2\'s preference list?')
-    .command('2 3 1')
+    .command('2 3 1', {
+        onCompleteDelay: 1500
+    })
     .respond('What is man 3\'s preference list?')
-    .command('1 3 2')
+    .command('1 3 2', {
+        onCompleteDelay: 1500
+    })
     .respond('What is woman 1\'s preference list?')
-    .command('2 3 1')
+    .command('2 3 1', {
+        onCompleteDelay: 1000
+    })
     .respond('What is woman 2\'s preference list?')
-    .command('2 1 3')
+    .command('2 1 3', {
+        onCompleteDelay: 1000
+    })
     .respond('What is woman 3\'s preference list?')
-    .command('3 2 1')
+    .command('3 2 1', {
+        onCompleteDelay: 2000
+    })
     .respond(response)
     .end();
